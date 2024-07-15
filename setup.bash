4966d155ba92b9c13c0cb353919df808758122f6
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2024-07-15 18:57:16 +0000

    Setup script for shadcn sveltekit

---
+++ setup.bash
@@ -0,0 +1,22 @@
+echo "Setup script is running for bash."
+
+npm create svelte@latest weights
+cd weights
+npx @svelte-add/tailwindcss@latest
+npm install
+
+echo "Making jsconfig.json file"
+touch jsconfig.json
+echo '{
+  "extends": "./.svelte-kit/tsconfig.json"
+}' > jsconfig.json
+
+echo "File made, ls: "
+ls
+
+npx shadcn-svelte@latest init
+
+echo "Installing components now: "
+npx shadcn-svelte@latest add
+
+# I should automate a lot of the key pressing etc, maybe git add everything then have it as template basically
